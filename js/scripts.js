e017477edff0570f4bf5e8270fed1048d29b61ea
Cameron Jensen and Stormi Hoebelheinrich <stormi@>
2017-02-13 11:51:20 -0800

    Add cups to pints conversion in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
-var cupsToLiters = function() { <!--Cups to Liters-->
+var cupsToLiters = function(){
   return cups * 0.23658824;
 };
 var cups = parseInt(prompt("How many cups?"));
 
 alert(cups + " cups equals " + cupsToLiters().toPrecision(4) + "liters.");
 
-var gramsToOunces = function () {<!--Gram to Ounces-->
+var gramsToOunces = function () {
   return grams * 0.03527396195;
 };
 
 var grams = parseInt(prompt("How many grams?"));
 
 alert(grams + " grams equals " + gramsToOunces().toPrecision(3) + " ounces.");
+
+var cupsToPints = function() {
+  return cups * .5;
+};
+var cups = parseInt(prompt("How many cups?"));
+
+alert(cups + " cups equal " + cupsToPints().toPrecision(4) + " Pints.");
